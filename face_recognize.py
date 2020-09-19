7e658ee5b51ad11c5ed458fef1e99e9b75c2cd66
ZhangEnshuo <1695313158@qq.com>
2020-09-19 10:49:07 +0800

    Add some comments

--- face_recognize.py
+++ face_recognize.py
@@ -7,13 +7,13 @@ from net.inception import InceptionResNetV1
 
 class face_rec():
     def __init__(self):
-        # 创建mtcnn对象
+        # 创建mtcnn对象，建立一个mtcnn模型
         # 检测图片中的人脸
         self.mtcnn_model = mtcnn()
         # 门限函数
         self.threshold = [0.5,0.8,0.9]
 
-        # 载入facenet
+        # 载入facenet模型
         # 将检测到的人脸转化为128维的向量
         self.facenet_model = InceptionResNetV1()
         # model.summary()
@@ -31,10 +31,13 @@ class face_rec():
 
         self.known_face_names=[]
 
+        # 所有的人脸进行遍历，得到仓库里所有人脸的128维特征向量和对应名称
         for face in face_list:
+            # obama.jpg进行'.'分割，取前一段字符串
             name = face.split(".")[0]
-
+            # 人脸读取出来
             img = cv2.imread("./face_dataset/"+face)
+            # 图像由 BGR -> RGB
             img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
             # 检测人脸
@@ -43,15 +46,16 @@ class face_rec():
             # 转化成正方形
             rectangles = utils.rect2square(np.array(rectangles))
             # facenet要传入一个160x160的图片
+            # 进行读取
             rectangle = rectangles[0]
-            # 记下他们的landmark
+            # 记下他们的landmark，5个标记点，进行人脸对齐
             landmark = (np.reshape(rectangle[5:15],(5,2)) - np.array([int(rectangle[0]),int(rectangle[1])]))/(rectangle[3]-rectangle[1])*160
-
+            #人脸截取
             crop_img = img[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
             crop_img = cv2.resize(crop_img,(160,160))
-
+            # 进行对齐操作
             new_img,_ = utils.Alignment_1(crop_img,landmark)
-
+            # 增加一个维度
             new_img = np.expand_dims(new_img,0)
             # 将检测到的人脸传入到facenet的模型中，实现128维特征向量的提取
             face_encoding = utils.calc_128_vec(self.facenet_model,new_img)
@@ -75,6 +79,7 @@ class face_rec():
 
         # 转化成正方形
         rectangles = utils.rect2square(np.array(rectangles,dtype=np.int32))
+        # 标记的点在宽和高的范围之内
         rectangles[:,0] = np.clip(rectangles[:,0],0,width)
         rectangles[:,1] = np.clip(rectangles[:,1],0,height)
         rectangles[:,2] = np.clip(rectangles[:,2],0,width)
@@ -86,19 +91,24 @@ class face_rec():
         for rectangle in rectangles:
             landmark = (np.reshape(rectangle[5:15],(5,2)) - np.array([int(rectangle[0]),int(rectangle[1])]))/(rectangle[3]-rectangle[1])*160
 
+            # 截出来照片
             crop_img = draw_rgb[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
             crop_img = cv2.resize(crop_img,(160,160))
 
+            # 对齐-矫正
             new_img,_ = utils.Alignment_1(crop_img,landmark)
             new_img = np.expand_dims(new_img,0)
 
+            # 传入facenet模型中，提取出128维的特征向量并保存在face_encodings中
             face_encoding = utils.calc_128_vec(self.facenet_model,new_img)
             face_encodings.append(face_encoding)
 
+        # 实时获得的人脸进行编码后的结果与数据库中所有的人脸进行对比，计算得分
         face_names = []
         for face_encoding in face_encodings:
             # 取出一张脸并与数据库中所有的人脸进行对比，计算得分
-            matches = utils.compare_faces(self.known_face_encodings, face_encoding, tolerance = 0.9)
+            # 两个不同的人脸特征值之差一般在1.4左右
+            matches = utils.compare_faces(self.known_face_encodings, face_encoding, tolerance = 0.9)# 可以改动的参数
             name = "Unknown"
             # 找出距离最近的人脸
             face_distances = utils.face_distance(self.known_face_encodings, face_encoding)
@@ -128,6 +138,7 @@ if __name__ == "__main__":
         ret, draw = video_capture.read()
         dududu.recognize(draw) 
         cv2.imshow('Video', draw)
+        # 图片显示
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break
 
